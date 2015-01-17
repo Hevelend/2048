86b2a36afaf31537a51f7af59aaccdd025dd9c03
Hevelend <nutella-ferrero@hotmail.fr>
2015-01-17 16:51:19 -0500

    ToString + Variables C2048

--- src/Lab0POO/C2048.java
+++ src/Lab0POO/C2048.java
@@ -1,5 +1,24 @@
 package Lab0POO;
 
 public class C2048 {
-
+	private int BoardGame[][] = new int[4][4];
+	private int Score = 0;
+	private boolean EndGame = true;
+	private int UselessVar = 0;
+	
+	public String toString(){
+		String Draw = "";
+		Draw = "---------";
+		for (int y=0; y < 3; y++){
+			Draw += "|";
+			for (int x=0; x < 3; x++)
+				switch(BoardGame[y][x]){
+				case -1 : Draw += "O";break;
+				case 0 : Draw += " ";break;
+				case 1 : Draw += "X";break;
+				}
+			Draw += "|\n";
+		}
+		return Draw;			
+	}
 }
